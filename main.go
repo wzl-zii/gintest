3e1b5b32683ea68d376888b9a801b98b4fe68b96
wzl-zii <3381145756@qq.com>
2025-11-22 20:29:27 +0800

    1

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		c.String(200, "hello world")
+		c.String(200, "hello world123")
 	})
 	r.Run()
 }
