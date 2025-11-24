272cd7326afe0c5573721c18292e2485bd5703d5
wzl-zii <3381145756@qq.com>
2025-11-24 17:52:25 +0800

    1

--- main.go
+++ main.go
@@ -2,10 +2,36 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+type AAA struct {
+	Title   string `json:"title"` //这种方法可以使显示出来的json小写
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		c.String(200, "hello world123")
+		c.String(200, "值：%v", "黄琴")
+	})
+	r.GET("/json1", func(c *gin.Context) {
+		c.JSON(200, map[string]interface{}{
+			"success": true,
+			"msg":     "hello world",
+		})
+	})
+	r.GET("/json2", func(c *gin.Context) {
+		c.JSON(200, map[string]interface{}{
+			"success": true,
+			"msg":     "hello world!!!!",
+		})
+	})
+	r.GET("/jsonp", func(c *gin.Context) { //jsonp，将callback值返回
+		a := &AAA{
+			Title:   "标题--jsonp",
+			Desc:    "描述一下",
+			Content: "测试内容",
+		}
+		c.JSONP(200, a)
 	})
 	r.Run()
 }
